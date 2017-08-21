f672a3a7a28dddd6dd80f0c475e85ac262c38878
xiadd <xiadd0102@gmail.com>
2017-08-21 18:03:07 +0800

    'build'

---
+++ test/basic.test.js
@@ -0,0 +1,15 @@
+const User = require('../services/user.js')
+const { expect } = require('chai')
+
+// 用户部分
+describe('services/user', function () {
+  it('通过id获取用户', async function () {
+    try {
+      const user = await User.findUserByName('xiadd')
+      expect(user).to.be.an('object')
+      expect(user.username).to.be.equal('xiadd')
+    } catch (err) {
+      done(err)
+    }
+  })
+})
--- test/user.test.js
+++
@@ -1,9 +0,0 @@
-const User = require('../services/user.js')
-const chai = require('chai')
-
-// 用户部分
-describe('services/user', function () {
-  describe('User.createUser', function () {
-    it.should()
-  })
-})
